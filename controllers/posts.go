b6a2d95c6cbdb5cffb37d1e4da91c8d3ff98521c
Jiraphon Wansai <jiraphonwansai@engineer.com>
2024-02-12 12:15:00 +0700

    go-api up03

--- controllers/posts.go
+++ controllers/posts.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jamesatomc/go-api/api/models"
+	"github.com/jamesatomc/go-api/models"
 )
 
 func CreatPost(c *gin.Context) {
--- controllers/users.go
+++ controllers/users.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	"github.com/jamesatomc/go-api/api/models"
+	"github.com/jamesatomc/go-api/models"
 	"gorm.io/gorm"
 )
 
